23b5bd2812f146b343d70f7cbd9e314d0d6a0da0
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:41:27 +0530

    Update save_system.py

--- systems/save_system.py
+++ systems/save_system.py
@@ -1,1 +1,47 @@
+import json
+import os
+from config import SAVE_FILE
 
+def load_saves():
+    if os.path.exists(SAVE_FILE):
+        return json.load(open(SAVE_FILE))
+    return {}
+
+def save_saves(data):
+    json.dump(data, open(SAVE_FILE, "w"))
+
+def choose_slot():
+    saves = load_saves()
+
+    for i in range(1,4):
+        if str(i) in saves:
+            print(f"{i}. {saves[str(i)]['name']}")
+        else:
+            print(f"{i}. Empty")
+
+    return input("Choose slot: ")
+
+def create_or_load_player(slot):
+    saves = load_saves()
+
+    if slot in saves:
+        return saves[slot]
+
+    name = input("Enter name: ")
+
+    player = {
+        "name": name,
+        "level": 1,
+        "score": 0,
+        "inventory": [],
+        "achievements": []
+    }
+
+    saves[slot] = player
+    save_saves(saves)
+    return player
+
+def save_progress(slot, player):
+    saves = load_saves()
+    saves[slot] = player
+    save_saves(saves)
