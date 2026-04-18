15abf9ac5fe0fd1010e80b99fc2f740bf9c3bab1
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:41:55 +0530

    Update cloud.py

--- systems/cloud.py
+++ systems/cloud.py
@@ -1,1 +1,7 @@
+import os
+from config import SAVE_FILE, CLOUD_FILE
 
+def cloud_save():
+    if os.path.exists(SAVE_FILE):
+        data = open(SAVE_FILE).read()
+        open(CLOUD_FILE, "w").write(data)
