3a16dc944191ddd55ef6eafca0d3fc92d115b146
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:40:58 +0530

    Update story.py

--- core/story.py
+++ core/story.py
@@ -1,1 +1,23 @@
+from utils.effects import slow_text
 
+def show_story(level, player):
+
+    if level == 1:
+        slow_text("""
+[WELCOME]
+
+"You are selected."
+Follow or go rogue?
+""")
+
+        c = input("1. Follow\n2. Rogue\n> ")
+        player["path"] = "loyal" if c == "1" else "rogue"
+
+    elif level == 3:
+        if player.get("path") == "rogue":
+            slow_text("You break rules...")
+        else:
+            slow_text("Stay in control...")
+
+    elif level == 5:
+        slow_text("⚠ AI SYSTEM ACTIVE")
