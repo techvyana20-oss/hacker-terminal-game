98b9d4fc71c7f52f55fb6d8c9113975024adaddd
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:42:53 +0530

    Update effects.py

--- utils/effects.py
+++ utils/effects.py
@@ -1,1 +1,9 @@
+import time
 
+def slow_text(text):
+    for line in text.strip().split("\n"):
+        for c in line:
+            print(c, end="", flush=True)
+            time.sleep(0.02)
+        print()
+        time.sleep(0.2)
