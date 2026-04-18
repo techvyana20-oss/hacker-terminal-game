41540e7df50069b03e616203edc0d1c7ae3ebf18
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:41:10 +0530

    Update boss.py

--- core/boss.py
+++ core/boss.py
@@ -1,1 +1,14 @@
+import random
 
+def boss_fight(player, level):
+    print("\n💀 BOSS LEVEL")
+
+    password = ''.join(str(random.randint(0,9)) for _ in range(6))
+
+    for _ in range(3):
+        if input("Boss password: ") == password:
+            print("BOSS DEFEATED")
+            return True
+        print("Wrong")
+
+    return False
