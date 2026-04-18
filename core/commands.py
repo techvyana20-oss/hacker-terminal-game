ce4c04371197c2ffc3e1844fa54f73d371134fe9
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:40:47 +0530

    Update commands.py

--- core/commands.py
+++ core/commands.py
@@ -1,1 +1,45 @@
+import random
 
+def command_mode(player, level):
+    connected = False
+    injected = False
+
+    while True:
+        cmd = input("hacker@system:~$ ").lower()
+
+        if cmd == "scan":
+            print("IP:", random.randint(100,255))
+
+        elif cmd == "connect":
+            connected = True
+            print("Connected")
+
+        elif cmd == "inject":
+            if connected:
+                injected = True
+                print("Injected")
+            else:
+                print("Connect first")
+
+        elif cmd == "decrypt":
+            if injected:
+                guess = input("Password: ")
+                if guess == player["current_password"]:
+                    print("ACCESS GRANTED")
+                    return True
+                else:
+                    print("Wrong")
+            else:
+                print("Inject first")
+
+        elif cmd == "inventory":
+            print(player["inventory"])
+
+        elif cmd == "help":
+            print("scan connect inject decrypt inventory exit")
+
+        elif cmd == "exit":
+            return False
+
+        else:
+            print("Unknown command")
