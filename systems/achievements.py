2f346a859bd62fbae7094218da1d8a82a1d8f7f2
techvyana20-oss <techvyana2.0@gmail.com>
2026-04-18 14:41:38 +0530

    Update achievements.py

--- systems/achievements.py
+++ systems/achievements.py
@@ -1,1 +1,13 @@
+from data.achievements_data import ACHIEVEMENTS
 
+def check_achievements(player):
+
+    if player["level"] >= 2 and "first" not in player["achievements"]:
+        player["achievements"].append("first")
+        player["inventory"].append("Scanner")
+        print("🏆", ACHIEVEMENTS["first"])
+
+    if player["level"] >= 5 and "pro" not in player["achievements"]:
+        player["achievements"].append("pro")
+        player["inventory"].append("Injector")
+        print("🏆", ACHIEVEMENTS["pro"])
